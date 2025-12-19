b84a126f58ee3a503e718fb492ac9a0a8beac3aa
HarshadSherekar <23uet111@dkte.ac.in>
2025-12-19 12:27:54 +0530

    Add files via upload

--- day7/D7asign1.py
+++ day7/D7asign1.py
@@ -8,7 +8,7 @@ server = Flask(__name__)
 def homepage():
     return"<html><body><h> this is a homepage <h><body><html> "
 
-@server.post('/reading/<int:id><float:temp><float:humidity>')
+@server.post('/reading')
 def createreading():
     id = request.form.get('id')
     temp = request.form.get('temp')
@@ -20,7 +20,28 @@ def createreading():
 
     return "values are added successfully"
 
+@server.get('/reading')
+def retrive():
+ query=" select * from senors;"
+ data=executeSelectQuery(query=query)
+ return f"senors:{data}"
 
+@server.put('/reading')
+def update():
+    id=request.form.get('id')
+    temp=request.form.get('temp')
+    query=f"update senors set id ={id} where temp={temp} "
+
+    executeQuery(query=query)
+    return " sensor id is updated successfully"
+
+
+@server.delete('/reading')
+def delete():
+    humidity=request.form.get('humidity')
+    query=f"delete from senors where humidity={humidity}"
+    executeQuery(query=query)
+    return"1 row deleted sucessfully"
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=4000, debug=True)
